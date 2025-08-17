f795d0d2c69fc417c4716a922e8d526dbec703c3
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-17 19:13:22 +0530

    1

---
+++ airplane.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+void securityScreening();
+void immigration();
+
+int main() {
+    securityScreening();
+    immigration();
+    return 0;
+}
+
