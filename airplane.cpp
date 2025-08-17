d7a0da102ef86e39653ea817df849de7eaae43fa
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-17 19:22:42 +0530

    2

--- airplane.cpp
+++ airplane.cpp
@@ -10,4 +10,35 @@ int main() {
     immigration();
     return 0;
 }
+void securityScreening() {
+    cout << "\nSecurity Screening\n";
 
+    char ans;
+    cout << "Any restricted articles ? (y/n): ";
+    cin >> ans; ans = tolower(ans);
+
+    if (ans == 'y') {
+        cout << "Give up restricted articles\n";
+    } else {
+        cout << "No restricted articles Proceed\n";
+    }
+
+    cout << "Metal detected? (y/n): ";
+    cin >> ans; ans = tolower(ans);
+
+    if (ans == 'y') {
+        cout << "Hand Search in progress\n";
+        cout << "Dangerous goods found? (y/n): ";
+        cin >> ans; ans = tolower(ans);
+
+        if (ans == 'y') {
+            cout << "Give up dangerous goods\n";
+        } else {
+            cout << "No dangerous goods Proceed\n";
+        }
+    } else {
+        cout << "No metal detected Proceed\n";
+    }
+
+    cout << "Security Screening Completed\n";
+}
